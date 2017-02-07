a8db429c68d8fd0c4f6d0472911dfc052b71191b
xuef <xuefeng@saidian.com>
2017-02-07 10:57:44 +0800

    add common

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 define("APP_PATH",  realpath(dirname(__FILE__) . '/../')); /* 指向public的上一级 */
+
+define('VIEW_PATH', APP_PATH . '/application/views/');
+
+define('LIB_PATH', APP_PATH . '/application/library/');
+
 $app  = new Yaf_Application(APP_PATH . "/conf/application.ini");
+
 $app->bootstrap()->run();
-?>
